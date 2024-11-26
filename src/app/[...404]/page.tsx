10a410b7d13dd45ba73ed9dc3de75711855f7190
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2024-11-26 11:12:44 -0500

    fix: build

--- src/app/[...404]/page.tsx
+++ src/app/[...404]/page.tsx
@@ -1,15 +1,15 @@
 import { notFound, redirect } from "next/navigation";
 
-export default async function NotFound({ params, searchParams }: { params: { [404]: string }, searchParams: Promise<Record<string, string | null>> }) {
+export default async function NotFound({ params, searchParams }: { params: Promise<{ [404]: string }>, searchParams: Promise<Record<string, string | null>> }) {
   // GET: https://qrt.badbird.dev/api/check?slug=ympc1
-  const res = await fetch(`https://qrt.badbird.dev/api/check?slug=${params[404]}`);
+  const resolvedParams = await params;
+  const res = await fetch(`https://qrt.badbird.dev/api/check?slug=${resolvedParams[404]}`);
   // if 200, redirect to the url
   // get the ref query param
   const ref = (await searchParams).ref ?? null;
   if (res.status === 200) {
-    return redirect(`https://qrt.badbird.dev/${params[404]}${ref ? `?ref=${encodeURIComponent(String(ref))}` : ""}`);
+    return redirect(`https://qrt.badbird.dev/${resolvedParams[404]}${ref ? `?ref=${encodeURIComponent(String(ref))}` : ""}`);
   }
 
   return notFound();
 }
-
