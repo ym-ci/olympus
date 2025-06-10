5582a80d3dad99693642f190ad21f580e382c0a3
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2025-06-10 18:14:26 -0400

    feat: add polymaker logo to robotics sponsors

--- src/app/(dark)/(clubs)/robotics/sponsors.tsx
+++ src/app/(dark)/(clubs)/robotics/sponsors.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import PolymakerLogo from "@/components/robotics/logos/polymaker";
 
 export default function Sponsors() {
   return (
@@ -9,9 +10,12 @@ export default function Sponsors() {
     >
       <div className="w-full max-w-7xl">
         <h2 className="mb-8 text-center text-3xl font-bold">Sponsors</h2>
-        <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center gap-8">
+          <Link href="https://polymaker.com/" target="_blank" className="hover:scale-105 transition-all duration-300 w-1/3 h-1/3">
+            <PolymakerLogo />
+          </Link>
           <Link href="https://spiritofmath.com/campus/don-mills/" target="_blank">
-            <Image src="/assets/robotics/sponsors/spirit-of-math.png" alt="Spirit of Math" width={300} height={300} className="hover:scale-105 transition-all duration-300" />
+            <Image src="/assets/robotics/sponsors/spirit-of-math.png" alt="Spirit of Math" width={250} height={250} className="hover:scale-105 transition-all duration-300" />
           </Link>
         </div>
       </div>
