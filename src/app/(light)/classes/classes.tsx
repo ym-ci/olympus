bea01db727789959ef0435247388566c2ed307af
mschan314 <57107641+mschan314@users.noreply.github.com>
2025-06-14 18:04:00 -0400

    Update classes.tsx

--- src/app/(light)/classes/classes.tsx
+++ src/app/(light)/classes/classes.tsx
@@ -222,6 +222,7 @@ const courses: Course[] = [
             <DriveVideo url="https://drive.google.com/file/d/1rNm4PYJbJTQRZtZnH-r7HbRD8gY0FjxH/preview"></DriveVideo>
             <DriveVideo url="https://drive.google.com/file/d/1AH44qgDeHXTR2Y5po2t2kkEnS0iUE8Wn/preview"></DriveVideo>
             <DriveVideo url="https://drive.google.com/file/d/1J30p7Sz_W45cfQB3pKRTi_tBeimk_zqT/preview"></DriveVideo>
+            <DriveVideo url="https://drive.google.com/file/d/1Ke1ocyj8MBuY370MW0uRx3eoIwnXb_-m/preview"></DriveVideo> {/* Adriano Tino */}       
           </div>
         ),
       },
@@ -258,6 +259,7 @@ const courses: Course[] = [
           <div className="flex w-full max-w-full flex-col overflow-x-auto md:gap-4">
             <YoutubeVideo id="Uiy7pGqdFlUs" />
             <DriveVideo url="https://drive.google.com/file/d/1SLakW7-nWj-e1OTyD2tMDsUCnkK1TS9B/preview"></DriveVideo>
+            <DriveVideo url="https://drive.google.com/file/d/12j98NuIUBUzMLfY_o9YhQreuynqHQQBw/preview"></DriveVideo> {/* Elemental Escape */}
           </div>
         ),
       },
