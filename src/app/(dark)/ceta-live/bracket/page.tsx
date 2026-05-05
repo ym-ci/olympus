2c503e4becd6f9c2d171496ad526dc61e70a0101
Aaron Huang <58890327+RunTheBot@users.noreply.github.com>
2026-05-04 21:25:23 -0400

    add Bracket component and update link href in CETALIVE component

--- src/app/(dark)/ceta-live/bracket/page.tsx
+++ src/app/(dark)/ceta-live/bracket/page.tsx
@@ -2,7 +2,7 @@ export default function Bracket() {
     // iframe to https://ceta.ymci.ca/bracket-svg
     return (
         <>
-            <iframe src="https://ceta.ymci.ca/bracket-svg" allowFullScreen />
+            <iframe class="w-full h-full" src="https://ceta.ymci.ca/bracket-svg" allowFullScreen />
         </>
     );
 }
