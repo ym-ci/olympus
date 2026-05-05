a61fb155ca959b5aac840778588ee85db2f715f2
Aaron Huang <58890327+RunTheBot@users.noreply.github.com>
2026-05-04 21:18:04 -0400

    aaa

---
+++ src/app/(dark)/ceta-live/braket/page.tsx
@@ -0,0 +1,8 @@
+export default function Bracket() {
+    // iframe to https://ceta.ymci.ca/bracket-svg
+    return (
+        <>
+            <iframe src="https://www.youtube.com/embed/19g66ezsKAg" allowFullScreen />
+        </>
+    );
+}
