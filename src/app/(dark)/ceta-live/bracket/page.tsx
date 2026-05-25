b4095a83699641a2e1144049d1266ec5be845912
Marie Karimizadeh <marie.karimizadeh@tdsb.on.ca>
2026-05-25 11:03:17 -0400

    removed CETA

--- src/app/(dark)/ceta-live/bracket/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Bracket() {
-    // iframe to https://ceta.ymci.ca/bracket-svg
-    return (
-        <>
-            <iframe style={{ width: '100%', height: '100vh' }} src="https://ceta.ymci.ca/bracket-svg" allowFullScreen />
-        </>
-    );
-}
--- src/app/(dark)/ceta/ui.tsx
+++
@@ -1,2 +0,0 @@
-export const gradient =
-  "bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-blue-800 text-transparent bg-clip-text font-bold";
